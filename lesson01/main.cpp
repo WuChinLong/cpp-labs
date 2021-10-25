673ab344d3a9ca19bee075deb74b076df2f853b7
EmersonWu <s897745@mail.yzu.edu.tw>
2021-10-25 18:56:40 +0800

    lesson完戶第一版

--- lesson01/main.cpp
+++ lesson01/main.cpp
@@ -2,8 +2,49 @@
 
 using namespace std;
 
+void callByValue(int num)
+{
+    num = 5;
+    cout << "in before callByValue: " << &num << endl;
+    cout << "in before callByValue: " << num << endl;
+
+}
+
+void callByAddress(int *pnum)
+{
+    *pnum =5;
+    cout << "in before callByAddress: " << pnum << endl;
+    cout << "in before callByAddress: " << *pnum << endl;
+
+}
+
+void callByReference(int &num)
+{
+    num = 9;
+    cout << "in before callByReference: " << &num << endl;
+    cout << "in before callByReference: " << num << endl;
+
+}
+
 int main()
 {
-    cout << "Hello world!" << endl;
+    int num =2;
+    cout << "num address: " << &num << endl;
+
+    cout << "before callByValue: " << num << endl;
+    callByValue(num);
+    cout << "after callByValue: " << num << endl;
+
+    cout << "before callByAddress: " << num << endl;
+    callByAddress(&num);
+    cout << "after callByAddress: " << num << endl;
+
+    cout << "before callByReference: " << num << endl;
+    callByReference(num);
+    cout << "after callByReference: " << num << endl;
+
+
     return 0;
 }
+
+
